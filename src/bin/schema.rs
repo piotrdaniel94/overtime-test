540d0b9d353a4aa3ccd44511ce2dea957d670e1d
piotrdaniel94 <piotrdaniel94@gmail.com>
2023-06-08 12:50:41 +0300

    Initial Commit

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use over_time_test::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
